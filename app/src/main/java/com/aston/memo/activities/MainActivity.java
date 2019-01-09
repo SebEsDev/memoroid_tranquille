421036cf329fe79cc4131b72c5b23bb70a79ec5e
SebEsDev <sebastien.fellenberg@gmail.com>
2019-01-09 16:00:07 +0100

    jordan <3

--- app/src/main/java/com/aston/memo/activities/MainActivity.java
+++ app/src/main/java/com/aston/memo/activities/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //startActivity();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
--- app/src/main/java/com/aston/memo/activities/TaskActivity.java
+++ app/src/main/java/com/aston/memo/activities/TaskActivity.java
@@ -1,6 +1,25 @@
 package com.aston.memo.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+import com.aston.memo.R;
+
+import info.hoang8f.android.segmented.SegmentedGroup;
 
 public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+        EditText title = findViewById(R.id.task_title);
+        SegmentedGroup priority = findViewById(R.id.task_priority);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
 }
