97b99a68b3525133e03855777868c54f020cf203
SebEsDev <sebastien.fellenberg@gmail.com>
2019-01-09 14:10:40 +0100

    firstcsqfdsqdsqdsqdsqd

---
+++ app/src/main/java/com/aston/memo/model/Task.java
@@ -0,0 +1,93 @@
+package com.aston.memo.model;
+
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task {
+
+    private long dateCreation;
+    private String title;
+    private int priority;
+    private String desc;
+    private boolean done;
+    private long dateModif;
+    private boolean isDeleted;
+    private long deadLine;
+    private int id;
+
+    public Task(String title, int priority) {
+        this.title = title;
+        this.priority = priority;
+        this.dateCreation = System.currentTimeMillis();
+        this.id = ThreadLocalRandom.current().nextInt();
+
+    }
+
+    public long getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(long dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public long getDateModif() {
+        return dateModif;
+    }
+
+    public void setDateModif(long dateModif) {
+        this.dateModif = dateModif;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public long getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(long deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
